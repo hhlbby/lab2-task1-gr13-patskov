a4fcba25680a8b313f027f464c5f4dba0afd704b
Tema <149879980+hhlbby@users.noreply.github.com>
2026-02-21 09:40:00 +0000

    third_highest was added

--- .ccls-cache/@home@runner@workspace/src@main.c
+++ .ccls-cache/@home@runner@workspace/src@main.c
@@ -3,16 +3,19 @@
 #include <stdlib.h>
 #define N 23
 void sort_desc(int a[], int n) {
-        for (int i = 0; i < n - 1; i++) {
-                for (int j = 0; j < n - i - 1; j++) {
-                        if (a[j] < a[j + 1]) {
-                                int temp = a[j];
-                                a[j] = a[j + 1];
-                                a[j + 1] = temp;
-                        }
-                }
-        }
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (a[j] < a[j + 1]) {
+        int temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+      }
+    }
+  }
 }
-int main(void) {
-        return 0;
+int third_highest(int a[], int n) {
+  sort_desc(a, n);
+  return a[2];
 }
+
+int main(void) { return 0; }
