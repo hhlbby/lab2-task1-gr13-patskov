192eea35192b0881e9b812520bc44afce42d7dda
Tema <149879980+hhlbby@users.noreply.github.com>
2026-02-21 09:40:28 +0000

    whoIsHigher was added

--- .ccls-cache/@home@runner@workspace/src@main.c
+++ .ccls-cache/@home@runner@workspace/src@main.c
@@ -17,5 +17,14 @@ int third_highest(int a[], int n) {
   sort_desc(a, n);
   return a[2];
 }
+int whoIsHigher(int a[], int b[], int n) {
+        int a_third = third_highest(a, n);
+        int b_third = third_highest(b, n);
+        if (a_third > b_third)
+                return 1;
+        else
+                return 2;
+}
+
 
 int main(void) { return 0; }
